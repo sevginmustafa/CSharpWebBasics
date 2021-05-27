a2922aaa8472027cadf8bedebd8d3b426328f54f
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-05-27 11:36:06 +0300

    Add exercise

---
+++ 04. MVC Introduction/SUS/MyFirstMvcApp/Controllers/Controller.cs
@@ -0,0 +1,42 @@
+﻿using SUS.HTTP;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public abstract class Controller
+    {
+        public HttpResponse View([CallerMemberName] string viewPath = null)
+        {
+            var layout = File.ReadAllText("Views/Shared/_Layout.cshtml");
+
+            var viewContent = File.ReadAllText("Views/" + this.GetType().Name.Replace("Controller", "/") + viewPath + ".cshtml");
+
+            var responseHtml = layout.Replace("@RenderBody()", viewContent);
+
+            var reponseAsBytes = Encoding.UTF8.GetBytes(responseHtml);
+
+            HttpResponse response = new HttpResponse("text/html", reponseAsBytes);
+
+            return response;
+        }
+
+        public HttpResponse ViewFile(string path, string contentType)
+        {
+            var responseFile = File.ReadAllBytes(path);
+
+            var response = new HttpResponse(contentType, responseFile);
+
+            return response;
+        }
+
+        public HttpResponse Redirect(string url)
+        {
+            var response = new HttpResponse(HttpStatusCode.Found);
+            response.Headers.Add(new Header("Location", url));
+
+            return response;
+        }
+    }
+}
---
+++ 04. MVC Introduction/SUS/SUS.HTTP/Cookie.cs
@@ -0,0 +1,28 @@
+﻿namespace SUS.HTTP
+{
+    public class Cookie
+    {
+        public Cookie(string cookiesAsString)
+        {
+            var cookieParts = cookiesAsString.Split(new char[] { '=' }, 2);
+
+            Name = cookieParts[0];
+            Value = cookieParts[1];
+        }
+
+        public Cookie(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}={Value}";
+        }
+    }
+}
